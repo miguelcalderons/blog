98ac5388d6c055371f36ba3576c583d60ce688be
Miguel Calderon <miguelcalderons@Miguels-MacBook-Pro.local>
2022-03-16 21:16:44 -0400

    first 3 parts

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use \App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get( '/', function () {
+//    $files = File::files( resource_path( "posts" ) );
+
+    $posts = Post::all();
+
+    return view( 'posts', [
+        'posts' => $posts
+    ] );
+} );
+
+Route::get( 'posts/{post}', function ( $slug ) {
+    return view( 'post', [
+        'post' => Post::findOrFail( $slug ),
+    ] );
+//
+} );
