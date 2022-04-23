b7dd95ddd6b17f5e73ae19900bd59b8cb68b9c7d
Miguel Calderon <miguelcalderons@Miguels-MacBook-Pro.local>
2022-04-23 13:30:58 -0400

    filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,19 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
 
-    protected $with = ['category', 'author'];
+    protected $with = [ 'category', 'author' ];
 
-    protected $guarded = [];
+    public function scopeFilter( $query, $filters ) {
+        $query->when( $filters['search'] ?? false, fn( $query, $search ) =>
+        $query->where( fn( $query ) => $query->where( 'title', 'like', '%' . $search . '%' )
+                                             ->orWhere( 'body', 'like', '%' . $search . '%' )
+        )
+        );
+
+        $query->when( $filters['category'] ?? false, fn( $query, $category ) => $query
+            ->whereHas( 'category', fn( $query ) => $query->where( 'slug', $category ) )
+        );
+
+        $query->when( $filters['author'] ?? false, fn( $query, $author ) => $query
+            ->whereHas( 'author', fn( $query ) => $query->where( 'username', $author ) )
+        );
+    }
 
     public function category() {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo( Category::class );
     }
 
     public function author() {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo( User::class, 'user_id' );
     }
 }
